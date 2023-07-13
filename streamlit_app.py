52411809bad0ce33610c2d04efef957f158feee8
a7317107 <139354609+a7317107@users.noreply.github.com>
2023-07-13 15:43:50 +0300

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 #create the function
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
 #New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
